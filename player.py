c10c7024a6bb7ec6b2d6573eae52943e75ba702b
AaronGr <ajg.dev92@gmail.com>
2025-06-10 18:52:49 -0400

    limit firing rate

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y, player_radius):
         super().__init__(x, y, player_radius)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -35,14 +36,19 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot(dt)
+            self.shoot()
+            self.timer = PLAYER_SHOOT_COOLDOWN
+
+        self.timer -= dt
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    def shoot(self, dt):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity *= PLAYER_SHOOT_SPEED
+    def shoot(self):
+        if self.timer <= 0:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity *= PLAYER_SHOOT_SPEED
+
 
